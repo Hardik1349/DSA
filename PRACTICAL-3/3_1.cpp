e8f2cef1c31c062c0013d30e2d261e6ceaeaea1d
HardikMakhija <makhijahardik22@gmail.com>
2026-08-17 23:38:57 +0530

    Refactor sorting algorithms and enhance output
    Refactored sorting methods and improved output formatting.

--- PRACTICAL-3/3_1.cpp
+++ PRACTICAL-3/3_1.cpp
@@ -1,64 +1,76 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int a[5] = {35,60,25,15,98};
-    int i,j,temp;
+int main() {
+    int a[5] = {35, 60, 25, 15, 98};
+    int i, j, temp;
     cout << "Sorting by Bubble Sort Method:" << endl;
     cout << endl;
-    cout << "Array before sorting"<< endl;
-    for(i=0;i<5;i++){
+    cout << "Array before sorting:" << endl;
+    for (i = 0; i < 5; i++) {
         cout << a[i] << " ";
     }
-    for(i=0;i<5;i++){
-        for(j=i+1;j<5;j++){
-            if(a[j]<a[i]){
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+    for (i = 0; i < 5 - 1; i++) {
+        for (j = 0; j < 5 - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
-    cout << "\nArray after sorting"<< endl;
-    for(i=0;i<5;i++){
+    cout << "\nArray after sorting:" << endl;
+    for (i = 0; i < 5; i++) {
         cout << a[i] << " ";
     }
-    cout << endl;
-    cout << endl;
+    cout << "\n\n";
+    int ar[5] = {38, 19, 68, 57, 29};
+    int b, c, tempp;
     cout << "Sorting by Selection Sort Method:" << endl;
     cout << endl;
-    int ar[5]={38,19,68,57,29};
-    int b,c,tempp;
-    cout << "Array before sorting"<< endl;
-    for(b=0;b<5;b++){
+    cout << "Array before sorting:" << endl;
+    for (b = 0; b < 5; b++) {
         cout << ar[b] << " ";
     }
     cout << endl;
-    for(b=0;b<5;b++){
+    for (b = 0; b < 5 - 1; b++) {
         int min = b;
-        for(c=b+1;c<5;c++){
-            if(ar[min]>ar[c]){
-                min=c;
+        for (c = b + 1; c < 5; c++) {
+            if (ar[c] < ar[min]) {
+                min = c;
             }
         }
-        tempp=ar[b];
-        ar[b]=ar[min];
-        ar[min]=tempp;  
+        tempp = ar[b];
+        ar[b] = ar[min];
+        ar[min] = tempp;
     }
-    cout << "Array after sorting"<< endl;
-    for(b=0;b<5;b++){
+    cout << "Array after sorting:" << endl;
+    for (b = 0; b < 5; b++) {
         cout << ar[b] << " ";
     }
-
+    cout << "\n\n";
+    int array[5] = {38, 19, 68, 57, 29};
+    int d, e, temporary;
     cout << "Sorting by Insertion Sort Method:" << endl;
     cout << endl;
-    int ar[5]={38,19,68,57,29};
-    int d,e,temp;
-    cout << "Array before sorting"<< endl;
-    for(d=0;d<5;d++){
-        cout << ar[d] << " ";
+    cout << "Array before sorting:" << endl;
+    for (d = 0; d < 5; d++) {
+        cout << array[d] << " ";
     }
     cout << endl;
-    
+    for (d = 1; d < 5; d++) {
+        temporary = array[d];
+        e = d - 1;
+        while (e >= 0 && array[e] > temporary) {
+            array[e + 1] = array[e];
+            e--;
+        }
+        array[e + 1] = temporary;
+    }
+    cout << "Array after sorting:" << endl;
+    for (d = 0; d < 5; d++) {
+        cout << array[d] << " ";
+    }
+    cout << endl;
     return 0;
 }
